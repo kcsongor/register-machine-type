069ea97afb2cbef9495e0488c196331b64cc930f
Csongor Kiss <kiss.csongor.kiss@gmail.com>
2016-04-04 18:04:03 +0200

    Add zipper utilities and make registers also a zipper

--- Machine.hs
+++ Machine.hs
@@ -3,23 +3,29 @@
 {-# LANGUAGE ExistentialQuantification #-}
 {-# LANGUAGE GADTs #-}
 {-# LANGUAGE TypeOperators #-}
+{-# LANGUAGE PolyKinds #-}
 {-# LANGUAGE UndecidableInstances #-}
 
 module Machine where
 
 import GHC.TypeLits
+import Data.Type.Bool
 
 data Machine where
-  M :: Label -> Nat -> [Reg] -> Instructions -> Machine
+  M :: Label -> Nat -> Registers -> Instructions -> Machine
 
 -- Zipper for (somewhat) more efficient instruction lookup
 data Instructions where
-  I :: [Instr] -> Instr -> [Instr] -> Instructions
+  Is :: [Instr] -> Instr -> [Instr] -> Instructions
+
+data Registers where
+  Rs :: [Reg] -> Reg -> [Reg] -> Registers
 
 -- |Initialise a universal register machine
 -- (TODO) pre: at least one instruction has to be given
 type family Init (regc :: Nat) (is :: [Instr]) :: Machine where
-  Init n (i ': is) = M (L 0) n (InitList n) (I '[] i is)
+  Init n (i ': is)
+    = M (L 0) n (Rs '[] (R 0) (InitList (n - 1))) (Is '[] i is)
 
 data Reg where
   R :: Nat -> Reg
@@ -32,7 +38,8 @@ data Instr where
   Dec  :: Reg -> Label -> Label -> Instr
   Halt :: Instr
 
-type I    = 'I
+type Is   = 'Is
+type Rs   = 'Rs
 type M    = 'M
 type R    = 'R
 type L    = 'L
@@ -47,25 +54,33 @@ zero :: Gadget r '[Dec r (L 0) (L 1), Halt]
 zero = Gadget
 
 type family Execute (m :: Machine) :: Machine where
-  Execute (M ip rc rs (I prev (Inc r label) next))
-    = M label rc rs (I prev (Inc r label) next)
-  Execute (M ip rc rs (I prev (Dec r label1 label2) next))
-    = M label1 rc rs (I prev (Dec r label1 label2) next)
+  Execute (M ip rc rs (Is prev (Inc r label) next))
+    = M label rc rs (Is prev (Inc r label) next)
+  Execute (M ip rc rs (Is prev (Dec r label1 label2) next))
+    = M label1 rc rs (Is prev (Dec r label1 label2) next)
   Execute m
     = m
 
--- Utilities for modifying register lists
-data Modify = Increment | Decrement
+-- Zipper utilities
+type family Jump from to zipper where
+  Jump from to z
+    = If (from <=? to)
+           (Right (to - from) z)
+           (Left  (from - to) z)
 
-type family ModifyAt (xs :: [Reg]) (at :: Nat) (m :: Modify) :: [Reg] where
-  ModifyAt '[] n m
-    = '[]
-  ModifyAt (R r ': rs) 0 'Increment
-    = R (r + 1) ': rs
-  ModifyAt (R r ': rs) 0 'Decrement
-    = R (r - 1) ': rs
-  ModifyAt (r ': rs) n m
-    = r ': ModifyAt rs (n - 1) m
+type family Left (by :: Nat) (zipper :: k) :: k where
+  Left 0 z = z
+  Left n (Is (p ': prevs) cur next)
+    = Left (n - 1) (Is prevs p (cur ': next))
+  Left n (Rs (p ': prevs) cur next)
+    = Left (n - 1) (Rs prevs p (cur ': next))
+
+type family Right (by :: Nat) (zipper :: k) :: k where
+  Right 0 z = z
+  Right by (Is prevs cur (n ': next))
+    = Right (by - 1) (Is (cur ': prevs) n next)
+  Right by (Rs prevs cur (n ': next))
+    = Right (by - 1) (Rs (cur ': prevs) n next)
 
 type family InitList (size :: Nat) :: [Reg] where
   InitList 0 = '[]
